fabc04d47eb474888c441ecbedcc61420277bc31
Garth Mortensen <mortensengarth@hotmail.com>
2023-06-27 20:18:38 -0500

    Add individual method invocations

--- setup_coordinator.py
+++ setup_coordinator.py
@@ -1,4 +1,3 @@
-import argparse
 from conda_manager import CondaManager
 from git_handler import GitHandler
 from ssh_creator import SSH_Creator
@@ -10,6 +9,39 @@ class SetupCoordinator:
         self.ssh_creator = SSH_Creator()
         self.git_handler = GitHandler()
         self.conda_manager = CondaManager()
+
+    def execute_ssh(self, passphrase: str) -> None:
+        """
+        execute ssh utility class.
+
+        Args:
+            passphrase (str): passphrase for generating ssh key.
+
+        Returns:
+            None
+        """
+        self.ssh_creator.create_ssh_file_ed25519(passphrase)
+
+    def execute_git(self) -> None:
+        """
+        execute git utility class.
+
+        Returns:
+            None
+        """
+        self.git_handler.handle_git_user_settings()
+
+    def execute_conda(self, env_name: str) -> None:
+        """
+        execute conda utility class.
+
+        Args:
+            env_name (str): conda environment name.
+
+        Returns:
+            None
+        """
+        self.conda_manager.create_conda_env(env_name)
 
     def execute_all(self, passphrase: str, env_name: str) -> None:
         """
@@ -29,13 +61,11 @@ class SetupCoordinator:
         self.conda_manager.create_conda_env(env_name)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Configure git, ssh, and conda env.')
-    parser.add_argument('--passphrase', type=str, nargs='?', help='passphrase to generate SSH key')
-    parser.add_argument('--env_name', type=str, nargs='?', help='conda env name')
-    args = parser.parse_args()
+passphrase = "iwantadog"
+env_name = "rstudio"
 
-    setup_coordinator = SetupCoordinator()
-    passphrase = args.passphrase
-    env_name = args.env_name
-    setup_coordinator.execute_all(passphrase, env_name)
+setup_coordinator = SetupCoordinator()
+# setup_coordinator.execute_ssh(passphrase)
+# setup_coordinator.execute_git()
+# setup_coordinator.execute_conda(env_name)
+setup_coordinator.execute_all(passphrase, env_name)
